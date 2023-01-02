794b88a66383a9e08791cedd6d88e0b33bcbe09f
Jacques <jamie.whiting2003@gmail.com>
2023-01-02 14:42:22 +0000

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ use std::{fs::File, io::{BufRead, BufReader}, time::{Duration, Instant}, thread:
 const THREADS: usize = 4;
 const PHASE_VALS: [i16; 7] = [0, 1, 1, 2, 4, 0, 0];
 const TPHASE: i32 = 24;
+const INIT_K: f32 = 0.4;
+const STEP_K: f32 = 0.001;
 
-#[derive(Debug)]
 struct Position {
     psts: [[usize; 16]; 2],
     counters: [usize; 2],
@@ -19,7 +20,43 @@ struct Stuff {
 }
 
 impl Position {
-    fn eval(&self, params: &[i16; 768]) -> i16 {
+    fn from_epd(s: &str) -> Self {
+        let commands: Vec<&str> = s.split("c9").map(|v| v.trim()).collect();
+        // parsing fen
+        let mut phase: i16 = 0;
+        let mut psts: [[usize; 16]; 2] = [[0; 16]; 2];
+        let mut counters: [usize; 2] = [0, 0];
+        let mut idx: usize = 63;
+        let fen: &str = commands[0].split_whitespace().collect::<Vec<&str>>()[0];
+        let rows: Vec<&str> = fen.split('/').collect();
+        for row in rows {
+            for ch in row.chars().rev() {
+                if ch == '/' { continue }
+                if !ch.is_numeric() {
+                    let idx2: usize = ['P','N','B','R','Q','K','p','n','b','r','q','k'].iter().position(|&element| element == ch).unwrap_or(6);
+                    let (col, pc): (usize, usize) = ((idx2 > 5) as usize, idx2 - 6 * ((idx2 > 5) as usize));
+                    psts[col][counters[col]] = pc * 64 + (idx ^ (56 * (col == 0) as usize));
+                    counters[col] += 1;
+                    phase += PHASE_VALS[pc];
+                    idx -= (idx > 0) as usize;
+                } else {
+                    let len: usize = ch.to_string().parse::<usize>().unwrap_or(8);
+                    idx -= (idx >= len) as usize * len;
+                }
+            }
+        }
+        phase = std::cmp::min(phase, TPHASE as i16);
+        // parsing result
+        let result: f32 = match commands[1] {
+            "\"1-0\";" => 1.0,
+            "\"1/2-1/2\";" => 0.5,
+            _ => 0.0
+        };
+        Self {psts, counters, phase, result}
+    }
+
+    #[inline]
+    fn eval(&self, params: &[i16; 768]) -> f32 {
         let p: i32 = self.phase as i32;
         let mut mg: i16 = 0;
         let mut eg: i16 = 0;
@@ -31,86 +68,25 @@ impl Position {
             mg -= params[self.psts[1][i]];
             eg -= params[self.psts[1][i] + 384];
         }
-        ((p * mg as i32 + (TPHASE - p) * eg as i32) / TPHASE) as i16
+        ((p * mg as i32 + (TPHASE - p) * eg as i32) / TPHASE) as f32
     }
 }
 
-fn parse_epd(s: &str) -> Position {
-    let commands: Vec<&str> = s.split("c9").map(|v| v.trim()).collect();
-    // parsing fen
-    let mut phase: i16 = 0;
-    let mut psts: [[usize; 16]; 2] = [[0; 16]; 2];
-    let mut counters: [usize; 2] = [0, 0];
-    let mut idx: usize = 63;
-    let fen: &str = commands[0].split_whitespace().collect::<Vec<&str>>()[0];
-    let rows: Vec<&str> = fen.split('/').collect();
-    for row in rows {
-        for ch in row.chars().rev() {
-            if ch == '/' { continue }
-            if !ch.is_numeric() {
-                let idx2: usize = ['P','N','B','R','Q','K','p','n','b','r','q','k'].iter().position(|&element| element == ch).unwrap_or(6);
-                let (col, pc): (usize, usize) = ((idx2 > 5) as usize, idx2 - 6 * ((idx2 > 5) as usize));
-                psts[col][counters[col]] = pc * 64 + (idx ^ (56 * (col == 0) as usize));
-                counters[col] += 1;
-                phase += PHASE_VALS[pc];
-                idx -= (idx > 0) as usize;
-            } else {
-                let len: usize = ch.to_string().parse::<usize>().unwrap_or(8);
-                idx -= (idx >= len) as usize * len;
-            }
-        }
+impl Stuff {
+    fn slice_error(&self, k: f32, i: usize, ppt: usize) -> f32 {
+        self.positions[i*ppt..(i + 1)*ppt].iter()
+            .fold(0.0, |err, p| err + (p.result - 1.0 / (1.0 + 10f32.powf(-k * p.eval(&self.params) / 100.0))).powi(2))
     }
-    phase = std::cmp::min(phase, TPHASE as i16);
-    // parsing result
-    let result: f32 = match commands[1] {
-        "\"1-0\";" => 1.0,
-        "\"1/2-1/2\";" => 0.5,
-        _ => 0.0
-    };
-    Position {psts, counters, phase, result}
 }
 
-#[inline(always)]
-fn sigmoid(k: f32, x: f32) -> f32 {
-    1.0 / (1.0 + 10f32.powf(- k * x))
-}
-
-fn calculate_error(k: f32, stuff: &Stuff) -> f32 {
+fn error(k: f32, stuff: &Stuff) -> f32 {
     let ppt: usize = stuff.positions.len() / THREADS;
-    let mut total_error: f32 = 0.0;
-    scope(|s: &Scope| {
-        let mut threads: Vec<ScopedJoinHandle<f32>> = Vec::new();
-        for i in 0..THREADS {
-            threads.push(s.spawn(move || {
-                let mut error: f32 = 0.0;
-                for pos in &stuff.positions[i*ppt..(i+1)*ppt] {
-                    error += (pos.result - sigmoid(k, pos.eval(&stuff.params) as f32 / 100.0)).powi(2);
-                }
-                error
-            }));
-        }
-        for thread in threads {
-            total_error += thread.join().unwrap()
-        }
-    });
+    let total_error: f32 = scope(|s: &Scope|
+        (0..THREADS).map(|i| s.spawn(move || stuff.slice_error(k, i, ppt)))
+            .collect::<Vec<ScopedJoinHandle<f32>>>().into_iter()
+            .fold(0.0, |err, p| err + p.join().unwrap())
+    );
     total_error / stuff.num
-}
-
-fn optimise_k(mut initial_guess: f32, step_size: f32, stuff: &Stuff) -> f32 {
-    let mut best_error: f32 = calculate_error(initial_guess, stuff);
-    let step: f32 = if calculate_error(initial_guess - step_size, stuff) < calculate_error(initial_guess + step_size, stuff) {
-        -step_size
-    }
-    else {
-        step_size
-    };
-    loop {
-        let new_error: f32 = calculate_error(initial_guess + step, stuff);
-        if new_error >= best_error {break}
-        initial_guess += step;
-        best_error = new_error;
-    }
-    initial_guess
 }
 
 fn print_psts(params: &[i16; 768]) {
@@ -138,8 +114,8 @@ fn main() {
         std::thread::sleep(Duration::from_secs(5));
         std::process::exit(1)
     });
-    for line in BufReader::new(file).lines(){
-        let pos: Position = parse_epd(&line.unwrap());
+    for line in BufReader::new(file).lines() {
+        let pos: Position = Position::from_epd(&line.unwrap());
         n += 1;
         stuff.positions.push(pos);
     }
@@ -149,28 +125,37 @@ fn main() {
 
     // OPTIMISING K VALUE
     time = Instant::now();
-    let k: f32 = optimise_k(0.4, 0.001, &stuff);
-    let mut best_error: f32 = calculate_error(k, &stuff);
+    let mut k: f32 = INIT_K;
+    let mut best_error: f32 = error(k, &stuff);
+    let step: f32 = if error(k - STEP_K, &stuff) < error(k + STEP_K, &stuff) {-STEP_K} else {STEP_K};
+    loop {
+        let new_error: f32 = error(k + step, &stuff);
+        if new_error >= best_error {break}
+        k += step;
+        best_error = new_error;
+    }
     println!("optimal k: {:.3}, error: {:.6}, time: {:.2}s", k, best_error, time.elapsed().as_millis() as f32 / 1000.0);
+    let k: f32 = k;
 
     // TEXEL TUNING
     let mut improves: [i16; 768] = [1; 768];
     let mut improved: bool = true;
     let mut count: i32 = 1;
+    let mut new_error: f32;
     while improved {
         time = Instant::now();
         improved = false;
         for (i, dir) in improves.iter_mut().enumerate() {
             stuff.params[i] += *dir;
-            let new_error: f32 = calculate_error(k, &stuff);
+            new_error = error(k, &stuff);
             if new_error < best_error {
                 best_error = new_error;
                 improved = true;
             } else {
                 stuff.params[i] -= 2 * (*dir);
-                let new_error2: f32 = calculate_error(k, &stuff);
-                if new_error2 < best_error {
-                    best_error = new_error2;
+                new_error = error(k, &stuff);
+                if new_error < best_error {
+                    best_error = new_error;
                     improved = true;
                     *dir = -(*dir);
                 } else {
