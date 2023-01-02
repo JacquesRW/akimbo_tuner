73b1cbb816d89b732745712d9b60f7dd3f0f2511
Jacques <jamie.whiting2003@gmail.com>
2023-01-02 17:22:32 +0000

    more cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use std::{cmp, fs::File, io::{self, BufRead}, process, time::Instant, thread};
 
-const SPARE_THREADS: usize = 0;
 const PIECE_CHARS: [char; 12] = ['P','N','B','R','Q','K','p','n','b','r','q','k'];
 const PHASE_VALS: [i16; 7] = [0, 1, 1, 2, 4, 0, 0];
 const TPHASE: i32 = 24;
-const INIT_K: f32 = 0.4;
-const STEP_K: f32 = 0.001;
+const INIT: f32 = 0.4;
+const STEP: f32 = 0.001;
 
 struct Position {
     psts: [[usize; 16]; 2],
@@ -30,10 +29,11 @@ impl Position {
             if ch == '/' { row -= 1; col = 0; }
             else if ('1'..='8').contains(&ch) { col += ch.to_string().parse::<i16>().unwrap_or(0) }
             else {
-                let idx2: usize = PIECE_CHARS.iter().position(|&element| element == ch).unwrap_or(6);
-                let c: usize = (idx2 > 5) as usize;
-                let pc: usize = idx2 - 6 * c;
-                pos.psts[c][pos.counters[c]] = pc * 64 + ((8 * row + col) as usize ^ (56 * (c == 0) as usize));
+                let idx: usize = PIECE_CHARS.iter().position(|&element| element == ch).unwrap_or(6);
+                let c: usize = (idx > 5) as usize;
+                let pc: usize = idx - 6 * c;
+                let sq: usize = (8 * row + col) as usize;
+                pos.psts[c][pos.counters[c]] = pc * 64 + (sq ^ (56 * (c ^ 1)));
                 pos.counters[c] += 1;
                 pos.phase += PHASE_VALS[pc];
                 col += 1
@@ -82,13 +82,16 @@ fn error(k: f32, stuff: &Stuff, num_threads: usize) -> f32 {
 }
 
 macro_rules! err {($s:expr) => {|_| {println!($s); process::exit(1);}}}
+macro_rules! elapsed {($time:expr) => {$time.elapsed().as_millis() as f32 / 1000.0}}
 
 fn main() {
-    let num_threads = thread::available_parallelism().unwrap_or_else(err!("error checking number of available threads")).get() - SPARE_THREADS;
-    println!("{} threads available, {} will be used", num_threads + SPARE_THREADS, num_threads);
+    let threads = thread::available_parallelism()
+        .unwrap_or_else(err!("error checking threads")).get();
+    println!("{threads} threads available");
     // LOADING POSITIONS
     let mut stuff: Stuff = Stuff {
-        params: [[[100; 64], [300; 64], [300; 64], [500; 64], [900; 64], [0; 64]]; 2].concat().concat().try_into().unwrap(),
+        params: [[[100; 64], [300; 64], [300; 64], [500; 64], [900; 64], [0; 64]]; 2]
+            .concat().concat().try_into().unwrap(),
         positions: Vec::new(),
         num: 0.0,
     };
@@ -96,44 +99,45 @@ fn main() {
     let mut n: usize = 0;
     let file: File = File::open("set.epd").unwrap_or_else(err!("error loading file"));
     for line in io::BufReader::new(file).lines() {
-        let pos: Position = Position::from_epd(&line.unwrap_or_else(err!("error reading line")));
+        let pos: Position = Position::from_epd(&line
+            .unwrap_or_else(err!("error reading line")));
         n += 1;
         stuff.positions.push(pos);
     }
-    let elapsed: u128 = time.elapsed().as_millis();
-    println!("loaded {n} positions in {} seconds ({}/sec)", elapsed as f32 / 1000.0, n * 1000 / elapsed as usize);
     stuff.num = n as f32;
+    let elapsed: f32 = elapsed!(time);
+    println!("loaded {n} positions in {elapsed} seconds ({}/sec)", stuff.num / elapsed);
 
     // OPTIMISING K VALUE
     time = Instant::now();
-    let mut k: f32 = INIT_K;
-    let mut best_error: f32 = error(k, &stuff, num_threads);
-    let step: f32 = if error(k - STEP_K, &stuff, num_threads) < error(k + STEP_K, &stuff, num_threads) {-STEP_K} else {STEP_K};
+    let mut k: f32 = INIT;
+    let mut best_error: f32 = error(k, &stuff, threads);
+    let mut new_error: f32;
+    let step: f32 = (error(k - STEP, &stuff, threads) - error(k + STEP, &stuff, threads)).signum() * STEP;
     loop {
-        let new_error: f32 = error(k + step, &stuff, num_threads);
-        if new_error >= best_error {break}
+        new_error = error(k + step, &stuff, threads);
+        if new_error > best_error {break}
         k += step;
         best_error = new_error;
     }
-    println!("optimal k: {k:.3}, error: {best_error:.6}, time: {:.2}s", time.elapsed().as_millis() as f32 / 1000.0);
+    println!("optimal k: {k:.3}, error: {best_error:.6}, time: {:.2}s", elapsed!(time));
 
     // TEXEL TUNING
     let mut improves: [i16; 768] = [1; 768];
     let mut improved: bool = true;
     let mut count: i32 = 1;
-    let mut new_error: f32;
     while improved {
         time = Instant::now();
         improved = false;
         for (i, dir) in improves.iter_mut().enumerate() {
             stuff.params[i] += *dir;
-            new_error = error(k, &stuff, num_threads);
+            new_error = error(k, &stuff, threads);
             if new_error < best_error {
                 best_error = new_error;
                 improved = true;
             } else {
                 stuff.params[i] -= 2 * (*dir);
-                new_error = error(k, &stuff, num_threads);
+                new_error = error(k, &stuff, threads);
                 if new_error < best_error {
                     best_error = new_error;
                     improved = true;
@@ -143,7 +147,7 @@ fn main() {
                 }
             }
         }
-        println!("epoch {count}: {:.2}s, error: {best_error:.6}", time.elapsed().as_millis() as f32 / 1000.0);
+        println!("epoch {count}: {:.2}s, error: {best_error:.6}", elapsed!(time));
         count += 1;
     }
     println!("Finished optimisation:");
