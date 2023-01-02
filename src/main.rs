319ad938707a4a6adf61e837150cc5a389f6baef
Jacques <jamie.whiting2003@gmail.com>
2023-01-02 22:58:26 +0000

    optimised epd parsing

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,14 @@ use std::{
     time::Instant,
 };
 
+const CHARS: [char; 12] = ['P', 'N', 'B', 'R', 'Q', 'K', 'p', 'n', 'b', 'r', 'q', 'k'];
 const TPHASE: i32 = 24;
 const NUM_PARAMS: usize = 768;
-const INIT: f32 = 0.4;
+const K: f32 = 0.4;
 const STEP: f32 = 0.001;
-const INITIAL_PARAMS: [[[i16; 64]; 6]; 2] = [[
+const INIT: [[i16; 64]; 6] = [
     [100; 64], [300; 64], [300; 64], [500; 64], [900; 64], [0; 64],
-]; 2];
+];
 
 struct Position {
     psts: [[u16; 16]; 2],
@@ -29,42 +30,39 @@ struct Data {
 }
 
 impl Position {
-    fn from_epd(s: &str) -> Self {
-        let mut commands = s.split("c9").map(str::trim);
-        let fen: &str = commands.next().expect("at least one part");
+    fn from_epd(epd: &str) -> Self {
         let mut pos = Position {
             psts: [[0; 16]; 2],
             counters: [0; 2],
             phase: 0,
-            result: match commands.next().expect("two parts") {
-                "\"1-0\";" => 1.0,
-                "\"0-1\";" => 0.0,
-                _ => 0.5,
-            },
+            result: 0.0,
         };
         let (mut row, mut col): (u16, u16) = (7, 0);
-        for ch in fen.chars() {
-            if ch == ' ' {
-                break;
-            } else if ch == '/' {
+        let mut divide: usize = 0;
+        for (i, ch) in epd.chars().enumerate() {
+            if ch == '/' {
                 row -= 1;
                 col = 0;
             } else if ('1'..='8').contains(&ch) {
                 col += ch.to_digit(10).expect("hard coded") as u16;
-            } else {
-                let idx: usize = ['P', 'N', 'B', 'R', 'Q', 'K', 'p', 'n', 'b', 'r', 'q', 'k']
-                    .iter()
-                    .position(|&element| element == ch)
-                    .expect("fen strings should be properly formatted");
+            } else if let Some(idx) = CHARS.iter().position(|&element| element == ch) {
                 let c: usize = idx / 6;
                 let (pc, sq): (u16, u16) = (idx as u16 - 6 * c as u16, 8 * row + col);
                 pos.psts[c][pos.counters[c] as usize] = pc * 64 + (sq ^ (56 * (c as u16 ^ 1)));
                 pos.counters[c] += 1;
                 pos.phase += [0, 1, 1, 2, 4, 0, 0][pc as usize];
                 col += 1;
+            } else if ch == ' ' {
+                divide = i;
+                break;
             }
         }
         pos.phase = cmp::min(pos.phase, TPHASE as i16);
+        pos.result = match epd[divide..].split_whitespace().last() {
+            Some("\"1-0\";") => 1.0,
+            Some("\"0-1\";") => 0.0,
+            _ => 0.5,
+        };
         pos
     }
 
@@ -99,21 +97,14 @@ fn error(k: f32, data: &Data) -> f32 {
     total / data.num
 }
 
-fn elapsed(time: &Instant) -> f32 {
+fn dur(time: &Instant) -> f32 {
     time.elapsed().as_millis() as f32 / 1000.0
 }
 
 fn main() {
-    let threads = available_parallelism().expect("available").get();
-    println!("{threads} threads available");
-
     // LOADING POSITIONS
     let mut data = Data {
-        params: INITIAL_PARAMS
-            .concat()
-            .concat()
-            .try_into()
-            .expect("hard coded"),
+        params: [INIT; 2].concat().concat().try_into().expect("hard coded"),
         positions: Vec::new(),
         num: 0.0,
         chunk_size: 0,
@@ -124,26 +115,20 @@ fn main() {
         data.positions.push(Position::from_epd(&ln.unwrap()));
         err + 1
     }) as f32;
-    data.chunk_size = data.positions.len() / threads;
-    let elapse = elapsed(&time);
-    println!(
-        "positions {} time {}s ({}/sec)",
-        data.num as u64,
-        elapse,
-        data.num / elapse
-    );
+    data.chunk_size = data.positions.len() / available_parallelism().expect("available").get();
+    println!("positions {:.0} ({}/sec)", data.num, data.num / dur(&time));
 
     // OPTIMISING K VALUE
     time = Instant::now();
-    let step: f32 = (error(INIT - STEP, &data) - error(INIT + STEP, &data)).signum() * STEP;
-    let mut k: f32 = INIT;
+    let step: f32 = (error(K - STEP, &data) - error(K + STEP, &data)).signum() * STEP;
+    let mut k: f32 = K;
     let (mut best, mut new): (f32, f32) = (error(k, &data), error(k + step, &data));
     while new <= best {
         k += step;
         best = new;
         new = error(k + step, &data);
     }
-    println!("k = {k:.3} time {}s error {best:.6}", elapsed(&time));
+    println!("time {}s error {best:.6} optimal k = {k:.3}", dur(&time));
 
     // TEXEL TUNING
     let mut cache: [i16; NUM_PARAMS] = [1; NUM_PARAMS];
@@ -169,7 +154,7 @@ fn main() {
                 }
             }
         }
-        println!("epoch time {}s error {best:.6}", elapsed(&time));
+        println!("time {}s error {best:.6}", dur(&time));
     }
     (0..12).for_each(|i| println!("{:?},", &data.params[i * 64..(i + 1) * 64]));
 
